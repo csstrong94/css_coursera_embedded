d5d836e0521307dcb3075756e4896f3ecf60d2ce
Chris Strong <csstrong@pm.me>
2021-03-20 22:52:49 -0600

    Filled out stubs and implemented each function

--- stats.h
+++ stats.h
@@ -24,21 +24,122 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
 
 /**
  * @brief Calculates min of array of integers
  *
- * <Add Extended Description Here>
+ * Finds and returns the minimum value of the data set
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param arr - a pointer to an array of integers
+ * @param len - length of the associated array
+ * 
+ * 
  *
- * @return <Add Return Informaiton here>
+ * @return an integer representing the minimum value in the array
  */
-int min(int* arr);
+unsigned char find_minimum(unsigned char* arr, unsigned int len);
+
+
+/**
+ * @brief Calculates max of array of integers
+ *
+ * Finds and returns the maximum value of the data set
+ *
+ * @param arr - a pointer to an array of integers 
+ * @param len - length of the associated array
+ * 
+ * 
+ *
+ * @return an integer representing the maximum value in the array
+ */
+
+unsigned char find_maximum(unsigned char* arr, unsigned int len);
+
+
+
+/**
+ * @brief Calculates mean of array of integers
+ *
+ * Finds and returns the average value of the data set
+ *
+ * @param arr - a pointer to an array of integers
+ * @param len - length of the associated array
+ * 
+ * 
+ *
+ * @return an integer representing the mean value in the array
+ */
+unsigned char find_mean(unsigned char* arr, unsigned int len);
+
+
+/**
+ * @brief Calculates median of array of integers
+ *
+ * Calculates and returns the median value of the data set
+ *
+ * @param arr - a pointer to an array of integers
+ * @param len - length of the associated array
+ * 
+ * 
+ *
+ * @return an integer representing the median value in the array
+ */
+unsigned char find_median(unsigned char* arr, unsigned int len);
+
+
+
+
+/**
+ * @brief Sorts the array from largest to smallest
+ *
+ * Sorts the dataset from largest to smallest
+ *
+ * @param arr - a pointer to an array of integers
+ * @param len - length of the associated array
+ * 
+ * 
+ *
+ * @return None
+ */
+void sort_array(unsigned char* arr, unsigned int len);
+
+
+
+/**
+ * @brief Prints the data set to the screen
+ *
+ * Prints the array to the screen
+ *
+ * @param arr - a pointer to an array of integers
+ * @param len - length of the associated array
+ * 
+ * 
+ *
+ * @return None
+ */
+void print_array(unsigned char* arr, unsigned int len);
+
+
+
+/**
+ * @brief Pretty prints statistics of the array
+ *
+ * Pretty prints the minimum, maximum, mean, and median
+ * of the supplied data set
+ *
+ * @param arr - a pointer to an array of integers
+ * @param len - length of the associated array
+ * 
+ * 
+ *
+ * @return None
+ */
+void print_statistics(unsigned char* arr, unsigned int len);
+
+
+
+
+
 
 
 
